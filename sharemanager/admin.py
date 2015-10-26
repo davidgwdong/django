61a13bbd539a5eadfd38802a8afbd1d700fd95f0
David Dong <ddong@blackberry.com>
2015-10-26 11:22:27 -0400

    add XPUser model for admin this change make customized user model showed in admin page

--- sharemanager/admin.py
+++ sharemanager/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin
+from sharemanager.models import XPUser
+
+class XPUserAdmin(UserAdmin):
+    model = XPUser
+
+    fieldsets = UserAdmin.fieldsets + (
+            (None, {'fields': ('gcm_token',)}),
+    )
+
+admin.site.register(XPUser, XPUserAdmin)
